158587331e32535cd4f1f192b62de70887f25bca
MatthiasBakken <36183902+MatthiasBakken@users.noreply.github.com>
2021-05-17 10:44:30 -0700

    completed all CRUD operations, moving onto frontend to finish connecting endpoints and handling data flow

--- server/index.js
+++ server/index.js
@@ -6,6 +6,16 @@ const pool = require( './db' );
 app.use( cors() );
 app.use( express.json() );
 
+app.get( "/todos", async ( request, response, next ) => {
+  try {
+    await pool.query( 'SELECT * FROM todo', ( err, res ) => {
+      response.json( res.rows );
+    } );
+  } catch ( error ) {
+    return next( error );
+  }
+} );
+
 app.post( "/todos", async ( request, response, next ) => {
   try {
     const { id, name, description, completed, subtasksIds, subtasks } = request.body;
@@ -31,6 +41,60 @@ app.post( "/todos", async ( request, response, next ) => {
   } catch ( error ) {
     console.log( error );
   };
+} );
+
+app.put( "/todos/:id", ( request, response, next ) => {
+  try {
+    const { id } = request.params;
+    const keys = [ 'name', 'description', 'completed' ];
+    const fields = [];
+
+    keys.forEach( key => {
+      if ( request.body[ key ] ) {
+        fields.push( key );
+      }
+    } );
+
+    fields.forEach( ( field, index ) => {
+      pool.query(
+        `UPDATE todo SET ${field}=($1) WHERE id=($2)`,
+        [ request.body[ field ], id ],
+        ( err, res ) => {
+          if ( err ) return next( err );
+
+          if ( index === fields.length - 1 ) response.json( { data: { success: "true", id: id } } );
+        }
+      );
+    } );
+  } catch ( error ) {
+    return next( error );
+  }
+} );
+
+app.delete( "/todos/:id", async ( request, response, next ) => {
+  try {
+    const { id } = request.params;
+    const { subtasksIds } = request.body;
+
+    await pool.query( 'DELETE FROM todo WHERE id=($1)', [ id ], ( err, res ) => {
+      if ( err ) return next( err );
+
+      console.log( subtasksIds );
+      if ( subtasksIds ) {
+      subtasksIds.forEach( id => {
+        pool.query(
+          'DELETE FROM subtasks WHERE id=($1)', [id],
+          ( err, res ) => {
+            if ( err ) return next( err );
+          }
+        )
+      })
+      }
+    } );
+    response.json( { data: { success: "true", id: id } } );
+  } catch ( error ) {
+    return next( error );
+  }
 } );
 
 app.listen( 5000, () => {
