b548456dc7579a29e0a59d4df07e96ddb0ac07da
MatthiasBakken <36183902+MatthiasBakken@users.noreply.github.com>
2021-05-17 01:40:40 -0700

    added a Form for creating todos with Formik and used Yup for validation

---
+++ src/components/TodoForm.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import { Formik, Form, Field } from 'formik';
+
+import { TodoFormSchema } from "./TodoFormSchema";
+import './TodoForm.css';
+
+
+const TODO = 'todo__';
+
+const TodoForm = ( props ) => {
+  // const { pageTitle } = props;
+
+  // useEffect( () => {
+  //   pageTitle( "CREATE NEW TODO" );
+  // }, [] );
+  
+  return (
+    <div className={`${TODO}form-container`} testid="form-container">
+      <Formik
+        initialValues={{
+          taskName: '',
+          description: '',
+          dateCreated: 0,
+        }}
+        validationSchema={TodoFormSchema}
+        onSubmit={( values ) => {
+          
+        }}
+      >
+        {( { errors, touched, isValid, dirty } ) => (
+          <Form className={`${TODO}form`} testid="todo-form">
+            <label testid="taskname-label">Todo: </label>
+            <Field
+              name="taskName"
+              className={`${TODO}input`}
+              testid="taskname-input"
+              placeholder="Do something"
+            />
+            {errors.taskName && touched.taskName ? (
+              <div className="error-div" testid="taskname-error">{errors.taskName}</div>
+            ) : null}
+            <label testid="description-label">Description: </label>
+            <Field
+              name="description"
+              className={`${TODO}input`}
+              testid="description-input"
+              placeholder="Walk over to shelf and grab a book to read"
+            />
+            {errors.description && touched.description ? (
+              <div className="error-div" testid="description-error">{errors.description}</div>
+            ) : null}
+            <span className={`${TODO}button-container`} testid="form-btn-container">
+              <button
+                className={`${TODO}submit-btn`}
+                type="submit"
+                disabled={!( dirty && isValid )}
+                testid="form-submit-btn"
+              >
+                ADD TODO
+              </button>
+            </span>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+export default TodoForm;
---
+++ src/components/TodoFormSchema.js
@@ -0,0 +1,12 @@
+import * as Yup from 'yup';
+
+
+export const TodoFormSchema = Yup.object().shape( {
+  taskName: Yup.string()
+    .min( 2, 'Length must be at least 2' )
+    .max( 30, 'Length must be less than 30' )
+    .required( 'Required' ),
+  description: Yup.string()
+    .min( 5, 'Length must be at least 5' )
+    .max( 150, 'Length must be less than 150' ),
+} );
